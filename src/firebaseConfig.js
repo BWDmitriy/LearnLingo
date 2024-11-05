1bd3f11bba476bdbd74410c4be981eba538b7e12
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-11-05 20:02:46 +0200

    checkpoint

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,19 +1,20 @@
-// src/firebaseConfig.js
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+   // src/firebaseConfig.js
+   import { initializeApp } from 'firebase/app';
+   import { getAuth } from 'firebase/auth';
+   import { getDatabase } from 'firebase/database';
 
-// Use environment variables to configure Firebase
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL, // Include this for Realtime Database
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-};
+   const firebaseConfig = {
+     apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+     authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+     databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+     projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+     storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+     messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+     appId: import.meta.env.VITE_FIREBASE_APP_ID,
+   };
 
-const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
+   const app = initializeApp(firebaseConfig);
+   const auth = getAuth(app);
+   const db = getDatabase(app);
 
-export { db };
+   export { auth, db };
