06db0f3cfd1b420c0cc049defd9bf2daff37eee0
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-11-10 16:02:41 +0200

    Teachers filter WiP

--- src/components/Teachers/Teachers.jsx
+++ src/components/Teachers/Teachers.jsx
@@ -17,7 +17,7 @@ function Teachers() {
       teachersRef,
       (snapshot) => {
         const data = snapshot.val();
-        console.log("Fetched data:", data); // Add this line to log the fetched data
+        console.log("Fetched data:", data);
         if (data) {
           const teachersList = Object.values(data);
           setTeachers(teachersList);
@@ -44,6 +44,39 @@ function Teachers() {
 
   return (
     <div className={styles.teachersPage}>
+      <div className={styles.filterDiv}>
+        <div>
+          <label>Languages</label>
+          <select>
+            <option defaultValue>French</option>
+            <option>English</option>
+            <option>German</option>
+            <option>Ukrainian</option>
+            <option>Polish</option>
+          </select>
+        </div>
+        <div>
+          <label>Level of knowledge</label>
+          <select>
+            <option defaultValue>A1 Beginner</option>
+            <option>A2 Elementary</option>
+            <option>B1 Intermediate</option>
+            <option>B2 Upper-Intermediate</option>
+            <option>C1 Advanced</option>
+            <option>C2 Proficient</option>
+          </select>
+        </div>
+        <div>
+          <label>Price</label>
+          <select>
+            <option>10 $</option>
+            <option>20 $</option>
+            <option defaultValue>30 $</option>
+            <option>40 $</option>
+            <option>50 $</option>
+          </select>
+        </div>
+      </div>
       <div className={styles.teacherList}>
         {teachers.slice(0, visibleTeachers).map((teacher, index) => (
           <TeacherCard key={index} teacher={teacher} />
