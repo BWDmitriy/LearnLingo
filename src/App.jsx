bc2599ef673770fcb7060d8483b88a8894afe4cf
Dmytro Zl <zl.dmitriy94@gmail.com>
2024-11-12 22:14:25 +0200

    App error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import Header from "./components/Header/Header";
-import { Suspense, lazy, useState } from "react";
+import { Suspense, lazy, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
 import "izitoast/dist/css/iziToast.min.css";
+import { useAuth } from "./hooks/useAuth";
 
 const Home = lazy(() => import("./components/Home/Home"));
 const Teachers = lazy(() => import("./components/Teachers/Teachers"));
@@ -11,10 +12,14 @@ const Favorites = lazy(() => import("./components/Favorites/Favorites"));
 
 function App() {
   const [isLogInOpen, setIsLogInOpen] = useState(false);
-
+  const { user } = useAuth();
   const openLogIn = () => setIsLogInOpen(true);
   const closeLogIn = () => setIsLogInOpen(false);
-
+  useEffect(() => {
+    if (!user) {
+      setIsLogInOpen(true); // Open login modal if user is not authenticated
+    }
+  }, [user]);
   return (
     <Router>
       <Header
@@ -26,7 +31,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/teachers" element={<Teachers />} />
-          <Route element={<PrivateRoute onOpenLogin={openLogIn} />}>
+          <Route element={<PrivateRoute />}>
             <Route path="/favorites" element={<Favorites />} />
           </Route>
         </Routes>
